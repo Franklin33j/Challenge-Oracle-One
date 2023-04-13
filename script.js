231bdd11477a2c0cde35501e7090946d0849119a
Franklin <angelguevaraj@gmail.com>
2023-04-13 13:03:44 -0600

    src updated for images

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ let encriptar=()=>{
    let title=document.getElementById('content-title')
    let description=document.getElementById('content-description')
    let cadEncriptada=''
+   let btnCopy=document.getElementById('btn-copy')
     //CADENA CON MAYUSCULAS
 
-   if(!validacionesComunes(info,title, description ))
+   if(!validacionesComunes(info,title, description, btnCopy ))
    {
        for (let index = 0; index < info.value.length; index++) {
       
@@ -36,6 +37,7 @@ let encriptar=()=>{
        title.innerText="La cadena ha sido encriptada con exito"
        description.innerHTML=`<strong>${info.value}</strong> <br> ${cadEncriptada}`
        info.value=''
+       btnCopy.style.display='block'
     }
     
 }
@@ -44,10 +46,11 @@ let desencriptar=()=>{
     let info= document.getElementById('data')
     let title=document.getElementById('content-title')
     let description=document.getElementById('content-description')
+    let btnCopy=document.getElementById('btn-copy')
     let cadDesencriptada=''
      //CADENA CON MAYUSCULAS
  
-    if(!validacionesComunes(info,title, description ))
+    if(!validacionesComunes(info,title, description, btnCopy ))
     {
         cadDesencriptada = info.value
         .replace(/enter/g, 'e')
@@ -58,11 +61,12 @@ let desencriptar=()=>{
         title.innerText="La cadena ha sido desencriptada con exito"
         description.innerHTML=`<strong>${info.value}</strong> <br> ${cadDesencriptada}`
         info.value=''
+        btnCopy.style.display='block'
    }
      
      }
     
-function validacionesComunes(info, title, description){
+function validacionesComunes(info, title, description, btnCopy){
     let valid=false;
     const regex1 = /[A-Z]/;
     const regex2= /[ÁÉÍÓÚÜÑ!?\-_@#$%&*(){}\[\]]/i;
@@ -72,19 +76,33 @@ function validacionesComunes(info, title, description){
       
         title.innerText="no se ha ingresado ningun valor"
         description.innerText="por favor ingrese un valor "
+        btnCopy.style.display='none'
         valid=true
     }
     //MAYUSCULAS
     else if (regex1.test(info.value)){
         title.innerText="Eror en la informacion ingresada!"
         description.innerText="La cadena no debe poseer caracteres en mayuscula "
+        btnCopy.style.display='none'
+        info.value =' '
         valid=true
     }
     //CARACTERES ESPECIALES
     else if (regex2.test(info.value)){
         title.innerText="Eror en la informacion ingresada!"
         description.innerText="La cadena no debe poseer caracteres especiales "
+        btnCopy.style.display='none'
+        info.value =' '
         valid=true
     }
     return valid
+}
+let copyText=()=>{
+    let description=document.getElementById('content-description')
+    navigator.clipboard.writeText(description.textContent)
+  .then(() => {
+  })
+  .catch(err => {
+    console.error('Error al copiar al portapapeles:', err)
+  })
 }
